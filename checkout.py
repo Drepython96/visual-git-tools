f3aac5213877f9298574ef3d966d4e6fc2b51c7c
Deandre Skelton <sdeandre@umich.edu>
2026-09-22 11:57:39 -0400

    Add temporary handling fee

--- checkout.py
+++ checkout.py
@@ -2,7 +2,8 @@ def calculate_total(price, quantity, tax_rate=0.06):
     """Calculate the purchase total including sales tax."""
     subtotal = price * quantity
     tax = subtotal * tax_rate
-    return round(subtotal + tax, 2)
+    total = subtotal + tax + 10
+    return round(total, 2)
 
 
 print(calculate_total(12.50, 2))
